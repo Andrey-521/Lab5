762daa9e06e6699ac4a6e19a3f40c5193b22d017
Andrey-521 <Lutcyk.andry@gmail.com>
2026-04-15 19:57:37 +0700

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,137 +1,143 @@
 #include <iostream>
-#include <vector>
-#include <stdexcept>
-#include <string>
+
 using namespace std;
 
-template<typename T>
+template <typename T>
 class Matrix {
-    size_t rows;
-    size_t cols;
-    vector<vector<T>> data;
+
+   T** data;
+   size_t rows, columns;
 
 public:
-    Matrix(): rows(0), cols(0) {}
-    Matrix(size_t r, size_t c, const T& init = T())
-        : rows(r), cols(c), data(r, vector<T>(c, init)) {}
-    Matrix(const Matrix& other)
-        : rows(other.rows), cols(other.cols), data(other.data) {}
-    ~Matrix() = default;
-
-    size_t Rows() const { return rows; }
-    size_t Cols() const { return cols; }
-
-    void Set(size_t i, size_t j, const T& value) {
-        if (i >= rows || j >= cols) throw out_of_range("Index out of range in Set");
-        data[i][j] = value;
+    Matrix(size_t r, size_t c) : rows(r), columns(c) {
+        data = new T*[rows];
+        for (size_t i = 0; i < rows; ++i) {
+            data[i] = new T[columns]();
+        }
     }
 
-    T Get(size_t i, size_t j) const {
-        if (i >= rows || j >= cols) throw out_of_range("Index out of range in Get");
-        return data[i][j];
+    Matrix(const Matrix& other) : rows(other.rows), columns(other.columns) {
+        data = new T*[rows];
+        for (size_t i = 0; i < rows; ++i) {
+            data[i] = new T[columns];
+            for (size_t j = 0; j < columns; ++j) {
+                data[i][j] = other.data[i][j];
+            }
+        }
+    }
+
+    Matrix& operator=(const Matrix& other) {
+        if (this == &other) return *this;
+
+        for (size_t i = 0; i < rows; ++i) {
+            delete[] data[i];
+        }
+        delete[] data;
+
+        rows = other.rows;
+        columns = other.columns;
+        data = new T*[rows];
+        for (size_t i = 0; i < rows; ++i) {
+            data[i] = new T[columns];
+            for (size_t j = 0; j < columns; ++j) {
+                data[i][j] = other.data[i][j];
+            }
+        }
+        return *this;
+    }
+
+    ~Matrix() {
+        for (size_t i = 0; i < rows; ++i) {
+            delete[] data[i];
+        }
+        delete[] data;
+    }
+
+    void Set(size_t r, size_t c, const T& value) {
+        if (r >= rows || c >= columns)
+            throw out_of_range("Index out of range");
+        data[r][c] = value;
+    }
+
+    T Get(size_t r, size_t c) const {
+        if (r >= rows || c >= columns)
+            throw out_of_range("Index out of range");
+        return data[r][c];
     }
 
     Matrix operator+(const Matrix& other) const {
-        if (rows != other.rows || cols != other.cols)
-            throw invalid_argument("Matrices must have same dimensions for addition");
+        if (rows != other.rows || columns != other.columns)
+            throw invalid_argument("Matrices must have the same dimensions for addition");
 
-        Matrix result(rows, cols);
-        for (size_t i = 0; i < rows; ++i)
-            for (size_t j = 0; j < cols; ++j)
-                result.data[i][j] = data[i][j] + other.data[i][j];
-        return result;
-    }
-
-    Matrix operator*(const Matrix& other) const {
-        if (cols != other.rows)
-            throw invalid_argument("Matrices dimensions incompatible for multiplication");
-
-        Matrix result(rows, other.cols, T());
+        Matrix result(rows, columns);
         for (size_t i = 0; i < rows; ++i) {
-            for (size_t j = 0; j < other.cols; ++j) {
-                T sum = T();
-                for (size_t k = 0; k < cols; ++k) {
-                    sum = sum + (data[i][k] * other.data[k][j]);
-                }
-                result.data[i][j] = sum;
+            for (size_t j = 0; j < columns; ++j) {
+                result.Set(i, j, this->Get(i, j) + other.Get(i, j));
             }
         }
         return result;
     }
 
-    template<typename U>
-    friend ostream& operator<<(ostream& os, const Matrix<U>& m);
+    Matrix operator*(const Matrix& other) const {
+        if (columns != other.rows)
+            throw invalid_argument("Matrices dimensions are not compatible for multiplication");
 
-    template<typename U>
-    friend istream& operator>>(istream& is, Matrix<U>& m);
-};
-
-template<typename U>
-ostream& operator<<(ostream& os, const Matrix<U>& m) {
-    os << m.rows << " " << m.cols << "\n";
-    for (size_t i = 0; i < m.rows; ++i) {
-        for (size_t j = 0; j < m.cols; ++j) {
-            os << m.data[i][j];
-            if (j + 1 < m.cols) os << " ";
+        Matrix result(rows, other.columns);
+        for (size_t i = 0; i < rows; ++i) {
+            for (size_t j = 0; j < other.columns; ++j) {
+                T sum = T();
+                for (size_t k = 0; k < columns; ++k) {
+                    sum += this->Get(i, k) * other.Get(k, j);
+                }
+                result.Set(i, j, sum);
+            }
         }
-        os << "\n";
+        return result;
     }
-    return os;
-}
 
-template<typename U>
-istream& operator>>(istream& is, Matrix<U>& m) {
-    size_t r, c;
-    if (!(is >> r >> c)) return is;
-    m.rows = r;
-    m.cols = c;
-    m.data.assign(r, vector<U>(c));
-    for (size_t i = 0; i < r; ++i)
-        for (size_t j = 0; j < c; ++j)
-            is >> m.data[i][j];
-    return is;
-}
+    friend ostream& operator<<(ostream& os, const Matrix& m) {
+        for (size_t i = 0; i < m.rows; ++i) {
+            for (size_t j = 0; j < m.columns; ++j) {
+                os << m.Get(i, j);
+                if (j + 1 < m.columns) os << " ";
+            }
+            os << endl;
+        }
+        return os;
+    }
+
+    friend istream& operator>>(istream& is, Matrix& m) {
+        for (size_t i = 0; i < m.rows; ++i)
+            for (size_t j = 0; j < m.columns; ++j)
+                is >> m.data[i][j];
+        return is;
+    }
+};
 
 int main() {
     try {
-        Matrix<int> A(2, 3);
-        A.Set(0,0,1); A.Set(0,1,2); A.Set(0,2,3);
-        A.Set(1,0,4); A.Set(1,1,5); A.Set(1,2,6);
+        Matrix<int> A(2, 2);
+        Matrix<int> B(2, 2);
 
-        Matrix<int> B(2, 3);
-        B.Set(0,0,10); B.Set(0,1,20); B.Set(0,2,30);
-        B.Set(1,0,40); B.Set(1,1,50); B.Set(1,2,60);
+        A.Set(0, 0, 1); A.Set(0, 1, 2);
+        A.Set(1, 0, 3); A.Set(1, 1, 4);
+        B.Set(0, 0, 5); B.Set(0, 1, 6);
+        B.Set(1, 0, 7); B.Set(1, 1, 8);
 
-        auto C = A + B;
-        cout << "A + B:\n" << C << endl;
+        cout << "Matrix A:\n" << A << "\n";
+        cout << "Matrix B:\n" << B << "\n";
 
-        Matrix<int> M1(2,3);
-        Matrix<int> M2(3,2);
+        Matrix<int> C = A + B;
+        cout << "A + B:\n" << C << "\n";
 
-        M1.Set(0,0,1); M1.Set(0,1,2); M1.Set(0,2,3);
-        M1.Set(1,0,4); M1.Set(1,1,5); M1.Set(1,2,6);
+        Matrix<int> D = A * B;
+        cout << "A * B:\n" << D << "\n";
 
-        M2.Set(0,0,7);  M2.Set(0,1,8);
-        M2.Set(1,0,9);  M2.Set(1,1,10);
-        M2.Set(2,0,11); M2.Set(2,1,12);
-
-        auto P = M1 * M2;
-        cout << "M1 * M2:\n" << P << endl;
-
-        Matrix<string> S1(2,2);
-        S1.Set(0,0,"a"); S1.Set(0,1,"b");
-        S1.Set(1,0,"c"); S1.Set(1,1,"d");
-
-        Matrix<string> S2(2,2);
-        S2.Set(0,0,"X"); S2.Set(0,1,"Y");
-        S2.Set(1,0,"Z"); S2.Set(1,1,"W");
-
-        auto Ssum = S1 + S2;
-        cout << "S1 + S2:\n" << Ssum << endl;
-
+    } catch (const exception& ex) {
+        cerr << "Error: " << ex.what() << "\n";
     }
-    catch (const exception& ex) {
-        cerr << "Error: " << ex.what() << endl;
-    }
+
+    return 0;
+}
     return 0;
 }
